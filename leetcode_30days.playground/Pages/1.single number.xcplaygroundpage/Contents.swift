75ebb0b9ead08098816acf88f19bf92cc2a85149
Jian Lv <lvjian700@gmail.com>
2020-04-11 22:24:39 +1000

    [1] using set as container.

--- leetcode_30days.playground/Pages/1.single number.xcplaygroundpage/Contents.swift
+++ leetcode_30days.playground/Pages/1.single number.xcplaygroundpage/Contents.swift
@@ -1,32 +1,14 @@
-import UIKit
+import Foundation
 
-/*:
- ## Single Number
- 
- <https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/>
-*/
 let nums = [4,1,2,1,2]
-let sorted = nums.sorted()
+var temp = Set<Int>()
 
-
-func findSingle(_ input: Array<Int>, lastIndex: Int) -> Int {
-  let first = input[lastIndex]
-
-  if lastIndex + 1 == input.count {
-    return first
+for n in nums {
+  if temp.contains(n) {
+    temp.remove(n)
+  } else {
+    temp.insert(n)
   }
-
-  print(input[lastIndex + 1])
-  let second = input[lastIndex + 1]
-  
-  if first != second {
-    return first
-  }
-
-  let next = lastIndex + 2
-  return findSingle(input, lastIndex: next)
 }
 
-
-
-findSingle(sorted, lastIndex: 0)
+temp.removeFirst()
