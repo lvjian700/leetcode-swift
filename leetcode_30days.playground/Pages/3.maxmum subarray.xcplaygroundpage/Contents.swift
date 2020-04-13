fd4cc76a017861377f45385a99bc65caec7b208f
Jian Lv <lvjian700@gmail.com>
2020-04-13 13:06:10 +1000

    [3] refactor

--- leetcode_30days.playground/Pages/3.maxmum subarray.xcplaygroundpage/Contents.swift
+++ leetcode_30days.playground/Pages/3.maxmum subarray.xcplaygroundpage/Contents.swift
@@ -2,23 +2,25 @@
 
 import Foundation
 
-let nums = [-2,1,-3,4,-1,2,1,-5,4]
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else {
+          return nums[0]
+        }
 
-func findMax(_ nums: [Int]) -> Int {
-    if nums.count == 1 { return nums[0] }
+        var ret = nums[0]
+        var currentSum = nums[0]
 
-    var ret = nums[0]
-    var currentSum = nums[0]
+        for i in 1..<nums.count {
+          currentSum = max(currentSum + nums[i], nums[i])
+          ret = max(ret, currentSum)
+        }
 
-    for i in 1..<nums.count {
-      currentSum = max(currentSum + nums[i], nums[i])
-      ret = max(ret, currentSum)
+        return ret
     }
-
-    return ret
 }
 
-findMax(nums)
-
+let array = [-2,1,-3,4,-1,2,1,-5,4]
+Solution().maxSubArray(array)
 
 //: [Next](@next)
