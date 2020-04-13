443c0feb3e20f3656f2bbe68b258fd3732fe3eb8
Jian Lv <lvjian700@gmail.com>
2020-04-13 23:01:34 +1000

    [3] refactor - recursion

--- leetcode_30days.playground/Pages/3.maxmum subarray.xcplaygroundpage/Contents.swift
+++ leetcode_30days.playground/Pages/3.maxmum subarray.xcplaygroundpage/Contents.swift
@@ -2,22 +2,35 @@
 
 import Foundation
 
+/*:
+ 1. Change to recursive sulotion
+ 2. Divide & conquer
+ */
+
 class Solution {
-    func maxSubArray(_ nums: [Int]) -> Int {
-        guard nums.count > 1 else {
-          return nums[0]
-        }
-
-        var ret = nums[0]
-        var currentSum = nums[0]
-
-        for i in 1..<nums.count {
-          currentSum = max(currentSum + nums[i], nums[i])
-          ret = max(ret, currentSum)
-        }
-
-        return ret
+  func maxSubArray(_ nums: [Int]) -> Int {
+    return findMax(nums, startAt: 1,
+                      currentSum: nums[0],
+                      currentMax: nums[0])
+  }
+  
+  private func findMax(_ nums: [Int], startAt index: Int, currentSum: Int, currentMax: Int) -> Int {
+    guard nums.count > 1 else {
+      return nums[0]
     }
+    
+    guard index < nums.endIndex else {
+      return currentMax
+    }
+    
+    let item = nums[index]
+    let sum = max(currentSum + item, item)
+    let maxNum = max(currentMax, sum)
+
+    return findMax(nums, startAt: index + 1,
+                      currentSum: sum,
+                      currentMax: maxNum)
+  }
 }
 
 let array = [-2,1,-3,4,-1,2,1,-5,4]
