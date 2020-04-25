30a172abfb7538daf6104bc0c427e7c7a3c25ccf
Jian Lv <lvjian700@gmail.com>
2020-04-26 09:59:12 +1000

    [18] initial setup

---
+++ leetcode_30days.playground/Pages/18.Minimum Path Sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## Minimum Path Sum
+ <https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3303/>
+ 
+ Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+ > You can only move either down or right at any point in time.
+ 
+ ```
+ Input:
+ [
+   [1,3,1],
+   [1,5,1],
+   [4,2,1]
+ ]
+ Output: 7
+ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ ```
+ */
+
+let input = [
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+
+class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
+      return 0
+    }
+}
+
+Solution().minPathSum(input) // expected 7
+
+//: [Next](@next)
