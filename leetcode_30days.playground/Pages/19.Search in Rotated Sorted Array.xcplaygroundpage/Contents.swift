47139f7395e89d944c53c81628cce5acafdca969
Jian Lv <lvjian700@gmail.com>
2020-04-26 21:01:21 +1000

    [19] setup

---
+++ leetcode_30days.playground/Pages/19.Search in Rotated Sorted Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## Search in Rotated Sorted Array
+ <https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3304/>
+ 
+ Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+ (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+ You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+ You may assume no duplicate exists in the array.
+
+ Your algorithm's runtime complexity must be in the order of O(log n).
+ 
+ */
+
+class Solution {
+  func search(_ nums: [Int], _ target: Int) -> Int {
+    return 0
+  }
+}
+
+Solution().search([4,5,6,7,0,1,2], 0) // 4
+Solution().search([4,5,6,7,0,1,2], 3) // -1
+
+//: [Next](@next)
