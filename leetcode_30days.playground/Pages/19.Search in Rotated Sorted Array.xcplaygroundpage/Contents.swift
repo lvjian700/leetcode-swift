2656268c54cbafa086d4a06baa8787fe6eda9ba9
Jian Lv <lvjian700@gmail.com>
2020-04-27 08:35:11 +1000

    [19] divide conquer

--- leetcode_30days.playground/Pages/19.Search in Rotated Sorted Array.xcplaygroundpage/Contents.swift
+++ leetcode_30days.playground/Pages/19.Search in Rotated Sorted Array.xcplaygroundpage/Contents.swift
@@ -18,9 +18,51 @@ import Foundation
  
  */
 
+
+
+/*
+ 0　　1　　2　　 4　　5　　6　　7
+
+ 7　　0　　1　　 2　　4　　5　　6
+
+ 6　　7　　0　　 1　　2　　4　　5
+
+ 5　　6　　7　　 0　　1　　2　　4
+
+ 4　　5　　6　　7　　0　　1　　2
+
+ 2　　4　　5　　6　　7　　0　　1
+
+ 1　　2　　4　　5　　6　　7　　0
+ */
 class Solution {
   func search(_ nums: [Int], _ target: Int) -> Int {
-    return 0
+    guard nums.count != 0 else {
+      return -1
+    }
+    
+    var left = 0, right = nums.count - 1
+
+    while left <= right {
+      let mid = left + (right - left)/2
+      if nums[mid] == target { return mid }
+
+      if(nums[mid] < nums[right]) {// right part ordered
+        if (target > nums[mid] && target <= nums[right]) { // Is target in right part?
+          left = mid + 1
+        } else {
+          right = mid - 1
+        }
+      } else { // left part ordered
+        if (target >= nums[left] && target < nums[mid]) {
+          right = mid - 1
+        } else {
+          left = mid + 1
+        }
+      }
+    }
+
+    return -1
   }
 }
 
