974c27964c6ab5bbb50557a3da4cc3d7f083d44d
Jian Lv <lvjian700@gmail.com>
2020-05-02 23:00:56 +1000

    [binary-tree] Setup binary tree playground

---
+++ explore/binary-tree.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## Binary Tree Preorder Traversal
+ <https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/>
+ 
+ */
+
+//: [Next](@next)
