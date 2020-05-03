b71cfe9c747b03dc8e4afc021733559d38c91a30
Jian Lv <lvjian700@gmail.com>
2020-05-03 21:55:01 +1000

    [btree] setup playground for inoder traversal

---
+++ explore/binary-tree.playground/Pages/2.Binary Tree Inorder Traversal .xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## Binary Tree Inorder Traversal
+ <https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/>
+ */
+
+class Solution {
+  func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    var ret: [Int] = []
+    
+    return ret
+  }
+}
+
+let root = TreeNode(1, left: nil,
+                      right: TreeNode(2, left: TreeNode(3, left: nil, right: nil), right: nil
+  )
+)
+
+Solution().inorderTraversal(root)
+
+//: [Next](@next)
