70c3765d66e78519d301d7e87ed67ce07f8ca4bf
Jian Lv <lvjian700@gmail.com>
2020-05-06 07:56:24 +1000

    [btree] postoder with iteration

--- explore/binary-tree.playground/Pages/3.Binary Tree Postorder Traversal.xcplaygroundpage/Contents.swift
+++ explore/binary-tree.playground/Pages/3.Binary Tree Postorder Traversal.xcplaygroundpage/Contents.swift
@@ -6,28 +6,28 @@ import Foundation
  ## Binary Tree Postorder Traversal
  https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/930/
  */
-
+/*
+ f -> b -> c -> d -> C -> e
+ */
 class Solution {
   func postorderTraversal(_ root: TreeNode?) -> [Int] {
     guard let root = root else {
       return []
     }
     
+    var stack: [TreeNode] = []
     var ret: [Int] = []
-    
-    func postorder(_ node: TreeNode) {
-      if let left = node.left {
-        postorder(left)
+    var prev: TreeNode? = root
+
+    while !stack.isEmpty || prev != nil {
+      if let p = prev {
+        stack.append(p)
+        ret.insert(p.val, at: 0)
+        prev = p.right
+      } else {
+        prev = stack.removeLast().left
       }
-      
-      if let right = node.right {
-        postorder(right)
-      }
-      
-      ret.append(node.val)
     }
-    
-    postorder(root)
     
     return ret
   }
@@ -37,6 +37,7 @@ let root = TreeNode(1, left: nil,
                       right: TreeNode(2, left: TreeNode(3, left: nil, right: nil), right: nil
   )
 )
+
 Solution().postorderTraversal(root) //[3,2,1]
 
 //: [Next](@next)
